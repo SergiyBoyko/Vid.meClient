25add895762366cec864934476dedbe6fefcc8e4
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-27 16:08:34 +0200

    Implemented onCreate(...) and onCreateView(...) methods of fragment

--- app/src/main/java/com/example/android/vidmeclient/ui/fragments/FeaturedFragment.java
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/FeaturedFragment.java
@@ -1,10 +1,27 @@
 package com.example.android.vidmeclient.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public class FeaturedFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
 }
