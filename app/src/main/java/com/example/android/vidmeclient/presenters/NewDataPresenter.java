28fea7be9861b6060432e21734d399efb8c9ea3f
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:26:13 +0200

    Added presenter for new videos

---
+++ app/src/main/java/com/example/android/vidmeclient/presenters/NewDataPresenter.java
@@ -0,0 +1,16 @@
+package com.example.android.vidmeclient.presenters;
+
+import com.example.android.vidmeclient.module.remote.INewDataSource;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class NewDataPresenter {
+
+    private final INewDataSource newDataSource;
+
+    public NewDataPresenter(INewDataSource newDataSource) {
+        this.newDataSource = newDataSource;
+    }
+}
