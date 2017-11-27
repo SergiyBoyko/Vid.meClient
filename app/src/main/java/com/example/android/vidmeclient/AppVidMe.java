f6a3977274a47da1c5a504f1a33df9f542ece923
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:34:54 +0200

    Implemented application class

--- app/src/main/java/com/example/android/vidmeclient/AppVidMe.java
+++ app/src/main/java/com/example/android/vidmeclient/AppVidMe.java
@@ -2,10 +2,28 @@ package com.example.android.vidmeclient;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.example.android.vidmeclient.di.component.AppComponent;
+import com.example.android.vidmeclient.di.component.DaggerAppComponent;
+import com.example.android.vidmeclient.di.module.ApiModule;
+import com.example.android.vidmeclient.di.module.AppModule;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public class AppVidMe extends MultiDexApplication {
-    
+    private AppComponent appComponent;
+
+    public AppVidMe() {
+        super();
+
+        appComponent = DaggerAppComponent.builder()
+                .apiModule(new ApiModule())
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent appComponent() {
+        return appComponent;
+    }
 }
