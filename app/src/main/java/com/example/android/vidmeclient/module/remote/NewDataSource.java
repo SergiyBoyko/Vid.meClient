ec922a4f2d7801dfa48961f0c3bc73c4ccf764bf
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:16:43 +0200

    Added module data source for new videos

---
+++ app/src/main/java/com/example/android/vidmeclient/module/remote/NewDataSource.java
@@ -0,0 +1,15 @@
+package com.example.android.vidmeclient.module.remote;
+
+import com.example.android.vidmeclient.api.VidMeApi;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class NewDataSource implements INewDataSource {
+    VidMeApi vidMeApi;
+
+    public NewDataSource(VidMeApi vidMeApi) {
+        this.vidMeApi = vidMeApi;
+    }
+}
