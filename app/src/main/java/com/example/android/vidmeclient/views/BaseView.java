fa8797bf2f495a96509a6de86f4e3a54db74e26d
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:28:03 +0200

    Added base view

---
+++ app/src/main/java/com/example/android/vidmeclient/views/BaseView.java
@@ -0,0 +1,13 @@
+package com.example.android.vidmeclient.views;
+
+import android.content.Context;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+
+}
