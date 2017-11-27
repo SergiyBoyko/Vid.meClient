8e9cdfa6e4ab7e6497f9af586a624e315df3c4d1
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:24:11 +0200

    Added presenter for featured videos

---
+++ app/src/main/java/com/example/android/vidmeclient/presenters/FeaturedDataPresenter.java
@@ -0,0 +1,16 @@
+package com.example.android.vidmeclient.presenters;
+
+import com.example.android.vidmeclient.module.remote.IFeaturedDataSource;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class FeaturedDataPresenter {
+
+    private final IFeaturedDataSource featuredDataSource;
+
+    public FeaturedDataPresenter(IFeaturedDataSource featuredDataSource) {
+        this.featuredDataSource = featuredDataSource;
+    }
+}
