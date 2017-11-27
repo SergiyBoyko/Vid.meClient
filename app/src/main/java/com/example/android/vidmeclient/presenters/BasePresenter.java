e2fbfddba7ca7418377a76fde383cbfdee1ae3a8
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:12:17 +0200

    Added base presenter

---
+++ app/src/main/java/com/example/android/vidmeclient/presenters/BasePresenter.java
@@ -0,0 +1,35 @@
+package com.example.android.vidmeclient.presenters;
+
+import com.example.android.vidmeclient.views.BaseView;
+
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class BasePresenter<T extends BaseView> {
+
+    private T view;
+
+    private CompositeSubscription compositeSubscription = new CompositeSubscription();
+
+    public T getView() {
+        return view;
+    }
+
+    public void setView(T view) {
+        this.view = view;
+    }
+
+    public void destroy() {
+        compositeSubscription.clear();
+    }
+
+    protected Subscription subscribe(Subscription subscription) {
+        compositeSubscription.add(subscription);
+        return subscription;
+    }
+
+}
