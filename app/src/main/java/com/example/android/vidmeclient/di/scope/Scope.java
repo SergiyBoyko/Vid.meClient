d5c36af57e6fdb777739d1ec0fb8d7cbc55803f7
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-27 22:13:11 +0200

    Realized scopes

--- app/src/main/java/com/example/android/vidmeclient/di/scope/Scope.java
+++ app/src/main/java/com/example/android/vidmeclient/di/scope/Scope.java
@@ -1,8 +1,16 @@
 package com.example.android.vidmeclient.di.scope;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
+@javax.inject.Scope
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Scope {
+
+    String value() default "";
+
 }
--- app/src/main/java/com/example/android/vidmeclient/di/scope/Scopes.java
+++ app/src/main/java/com/example/android/vidmeclient/di/scope/Scopes.java
@@ -5,4 +5,17 @@ package com.example.android.vidmeclient.di.scope;
  */
 
 public class Scopes {
+
+    /**
+     * Lifecycle scope annotation constants.
+     */
+    public static final String ACTIVITY = "activity";
+    public static final String SERVICE = "service";
+    public static final String FRAGMENT = "fragment";
+    public static final String VIEW = "view";
+
+    private Scopes() {
+        throw new AssertionError("Unable to instantiate");
+    }
+
 }
