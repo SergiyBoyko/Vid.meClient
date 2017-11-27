124b7388ab764c605747bbed4789c654c6e3834f
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:14:51 +0200

    Added module data source for featured viedos

---
+++ app/src/main/java/com/example/android/vidmeclient/module/remote/FeaturedDataSource.java
@@ -0,0 +1,15 @@
+package com.example.android.vidmeclient.module.remote;
+
+import com.example.android.vidmeclient.api.VidMeApi;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class FeaturedDataSource implements IFeaturedDataSource {
+    VidMeApi vidMeApi;
+
+    public FeaturedDataSource(VidMeApi vidMeApi) {
+        this.vidMeApi = vidMeApi;
+    }
+}
