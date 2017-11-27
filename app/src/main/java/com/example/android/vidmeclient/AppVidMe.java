3d3123e407a493d321323d1f111ce91f76462751
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-27 15:44:12 +0200

    Added application class

---
+++ app/src/main/java/com/example/android/vidmeclient/AppVidMe.java
@@ -0,0 +1,11 @@
+package com.example.android.vidmeclient;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class AppVidMe extends MultiDexApplication {
+    
+}
