bd415541856f70daf19e805bfcf1090bdca79b54
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 00:17:19 +0200

    Added module data source for logIning

---
+++ app/src/main/java/com/example/android/vidmeclient/module/remote/LogInDataSource.java
@@ -0,0 +1,15 @@
+package com.example.android.vidmeclient.module.remote;
+
+import com.example.android.vidmeclient.api.VidMeApi;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class LogInDataSource implements ILogInDataSource {
+    VidMeApi vidMeApi;
+
+    public LogInDataSource(VidMeApi vidMeApi) {
+        this.vidMeApi = vidMeApi;
+    }
+}
