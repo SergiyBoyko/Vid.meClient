fb8be7952d2a15af7beb28e26d4d03614dfb1e63
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-27 16:01:31 +0200

    Added FeaturedFragment class with layout file

---
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/FeaturedFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.vidmeclient.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by fbrsw on 27.11.2017.
+ */
+
+public class FeaturedFragment extends Fragment {
+}
