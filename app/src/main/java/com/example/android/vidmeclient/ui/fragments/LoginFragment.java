be6e861d7d9ad8f81a61d051072736751d6daff3
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-27 21:49:16 +0200

    Added offline username and pass validation

--- app/src/main/java/com/example/android/vidmeclient/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/LoginFragment.java
@@ -1,8 +1,133 @@
 package com.example.android.vidmeclient.ui.fragments;
 
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.vidmeclient.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
-public class LoginFragment {
+public class LoginFragment extends Fragment {
+    private static final int REQUEST_SIGNUP = 0;
+
+    @BindView(R.id.input_username)
+    EditText usernameText;
+    @BindView(R.id.input_password)
+    EditText passwordText;
+    @BindView(R.id.btn_login)
+    Button loginButton;
+    @BindView(R.id.link_signup)
+    TextView signUpLink;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, view);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                showText("SignUP");
+//                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+//                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+
+        return view;
+    }
+
+    private void login() {
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+//        loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        showText("login with " + username + " " + password);
+
+
+        // TODO: Implement authentication logic here.
+
+    }
+
+    public void onLoginSuccess() {
+        loginButton.setEnabled(true);
+//        finish();
+    }
+
+    public void onLoginFailed() {
+        showText("Login failed");
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (username.isEmpty() || username.length() < 4) {
+            usernameText.setError("minimum 4 symbols");
+            valid = false;
+        } else if (Character.isDigit(username.charAt(0))) {
+            usernameText.setError("username cannot begin with digit");
+            valid = false;
+        } else {
+            usernameText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4) {
+            passwordText.setError("minimum 4 symbols");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return valid;
+    }
+
+    private void showText(String text) {
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+    }
 }
