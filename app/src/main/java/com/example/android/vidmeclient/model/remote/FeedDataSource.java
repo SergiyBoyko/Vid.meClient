246b5aed5aef9a737ead39d0bba1366e6cc4c00a
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 21:36:07 +0200

    Implemented data source for feed content

--- app/src/main/java/com/example/android/vidmeclient/model/remote/FeedDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/FeedDataSource.java
@@ -1,6 +1,9 @@
 package com.example.android.vidmeclient.model.remote;
 
 import com.example.android.vidmeclient.api.VidMeApi;
+import com.example.android.vidmeclient.model.entities.ContentResponse;
+
+import rx.Observable;
 
 /**
  * Created by fbrsw on 27.11.2017.
@@ -11,5 +14,10 @@ public class FeedDataSource implements IFeedDataSource {
 
     public FeedDataSource(VidMeApi vidMeApi) {
         this.vidMeApi = vidMeApi;
+    }
+
+    @Override
+    public Observable<ContentResponse> getFeedContent(int lim, int off) {
+        return vidMeApi.getFeedContent(lim, off);
     }
 }
--- app/src/main/java/com/example/android/vidmeclient/model/remote/IFeedDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/IFeedDataSource.java
@@ -1,8 +1,14 @@
 package com.example.android.vidmeclient.model.remote;
 
+import com.example.android.vidmeclient.model.entities.ContentResponse;
+
+import rx.Observable;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface IFeedDataSource {
+
+    Observable<ContentResponse> getFeedContent(int lim, int off);
 }
