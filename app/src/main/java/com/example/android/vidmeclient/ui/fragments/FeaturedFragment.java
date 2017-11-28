c2c9bb024212ede2eccce6c12b0fee26dd4a4513
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 18:40:58 +0200

    Added test fragment implemention to get ten featured videos

--- app/src/main/java/com/example/android/vidmeclient/ui/fragments/FeaturedFragment.java
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/FeaturedFragment.java
@@ -1,20 +1,46 @@
 package com.example.android.vidmeclient.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.vidmeclient.AppVidMe;
 import com.example.android.vidmeclient.R;
+import com.example.android.vidmeclient.di.component.AppComponent;
+import com.example.android.vidmeclient.di.component.DaggerPresentersComponent;
+import com.example.android.vidmeclient.di.module.PresentersModule;
+import com.example.android.vidmeclient.model.entities.Video;
+import com.example.android.vidmeclient.presenters.FeaturedDataPresenter;
 import com.example.android.vidmeclient.views.FeaturedContentView;
+import com.example.android.vidmeclient.widgets.adapters.VideoContentAdapter;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public class FeaturedFragment extends Fragment implements FeaturedContentView {
+
+    private VideoContentAdapter adapter;
+
+
+    @BindView(R.id.my_recycler_view)
+    RecyclerView recyclerView;
+
+    @Inject
+    FeaturedDataPresenter presenter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,9 +50,39 @@ public class FeaturedFragment extends Fragment implements FeaturedContentView {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.featured_fragment, container, false);
+        View view = inflater.inflate(R.layout.featured_fragment, container, false);
+        ButterKnife.bind(this, view);
 
-        return v;
+        DaggerPresentersComponent.builder()
+                .appComponent(getAppComponent())
+                .presentersModule(new PresentersModule())
+                .build()
+                .inject(this);
+
+        adapter = new VideoContentAdapter(getContext(), null);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        presenter.setView(this);
+        presenter.getFeaturedContent(10, 0);
+        return view;
     }
 
+    @Override
+    public void showVideoContents(List<Video> videos) {
+        adapter.setVideos(videos);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public Context getContext() {
+        return getActivity().getApplicationContext();
+    }
+
+
+    public AppComponent getAppComponent() {
+        return ((AppVidMe) getActivity().getApplication()).appComponent();
+    }
 }
