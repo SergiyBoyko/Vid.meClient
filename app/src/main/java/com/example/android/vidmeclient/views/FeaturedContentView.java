0669b8c661524cd67a16cec86536bad855233cb0
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 18:37:09 +0200

    Added method for upating featured video

--- app/src/main/java/com/example/android/vidmeclient/views/FeaturedContentView.java
+++ app/src/main/java/com/example/android/vidmeclient/views/FeaturedContentView.java
@@ -1,8 +1,15 @@
 package com.example.android.vidmeclient.views;
 
+import com.example.android.vidmeclient.model.entities.Video;
+
+import java.util.List;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface FeaturedContentView extends BaseView {
+
+    void showVideoContents(List<Video> videos);
+
 }
