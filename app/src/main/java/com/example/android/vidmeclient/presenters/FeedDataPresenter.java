5b6b99115159475d7d5854985864055cc81a1a41
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 21:37:42 +0200

    Implemented getting feed videos

--- app/src/main/java/com/example/android/vidmeclient/presenters/FeedDataPresenter.java
+++ app/src/main/java/com/example/android/vidmeclient/presenters/FeedDataPresenter.java
@@ -1,16 +1,34 @@
 package com.example.android.vidmeclient.presenters;
 
+import com.example.android.vidmeclient.model.entities.ContentResponse;
 import com.example.android.vidmeclient.model.remote.IFeedDataSource;
+import com.example.android.vidmeclient.utils.rx.RxErrorAction;
+import com.example.android.vidmeclient.utils.rx.RxRetryWithDelay;
+import com.example.android.vidmeclient.views.FeedContentView;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
-public class FeedDataPresenter {
+public class FeedDataPresenter extends BasePresenter<FeedContentView> {
 
     private final IFeedDataSource feedDataSource;
 
     public FeedDataPresenter(IFeedDataSource feedDataSource) {
         this.feedDataSource = feedDataSource;
     }
+
+    public void getFeedContent(int lim, int off) {
+        subscribe(feedDataSource.getFeedContent(lim, off)
+                .retryWhen(new RxRetryWithDelay())
+                .map(ContentResponse::getVideos)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getView()::showVideoContent, new RxErrorAction(getView().getContext()))
+        );
+    }
+
 }
