0c8413a93225b606940c6235b21a58966913ba63
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 18:38:57 +0200

    Implemented adapter

--- app/src/main/java/com/example/android/vidmeclient/widgets/adapters/VideoContentAdapter.java
+++ app/src/main/java/com/example/android/vidmeclient/widgets/adapters/VideoContentAdapter.java
@@ -3,11 +3,11 @@ package com.example.android.vidmeclient.widgets.adapters;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.android.vidmeclient.R;
 import com.example.android.vidmeclient.model.entities.Video;
 
@@ -38,7 +38,13 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.videoName.setText(videos.get(position).getTitle());
+        holder.videoLikesCount.setText(String.valueOf(videos.get(position).getLikesCount()));
 
+        Glide
+                .with(context)
+                .load(videos.get(position).getThumbnailUrl())
+                .into(holder.videoImage);
     }
 
     @Override
@@ -47,13 +53,17 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
         return videos.size();
     }
 
+    public void setVideos(List<Video> videos) {
+        this.videos = videos;
+    }
+
     class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.card_text)
         TextView videoName;
         @BindView(R.id.card_image)
         ImageView videoImage;
-        @BindView(R.id.favourite_count)
-        TextView videoLikeCount;
+        @BindView(R.id.like_count)
+        TextView videoLikesCount;
 
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_video, parent, false));
