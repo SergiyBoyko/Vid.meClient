eca4950eb05882309fa89db9f4778b95a56ba2c7
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 18:30:09 +0200

    Added dataSource metod to get featured content

--- app/src/main/java/com/example/android/vidmeclient/model/remote/FeaturedDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/FeaturedDataSource.java
@@ -1,6 +1,9 @@
 package com.example.android.vidmeclient.model.remote;
 
 import com.example.android.vidmeclient.api.VidMeApi;
+import com.example.android.vidmeclient.model.entities.FeaturedContentResponse;
+
+import rx.Observable;
 
 /**
  * Created by fbrsw on 27.11.2017.
@@ -11,5 +14,10 @@ public class FeaturedDataSource implements IFeaturedDataSource {
 
     public FeaturedDataSource(VidMeApi vidMeApi) {
         this.vidMeApi = vidMeApi;
+    }
+
+    @Override
+    public Observable<FeaturedContentResponse> getFeaturedContent(int lim, int off) {
+        return vidMeApi.getFeaturedContent(lim, off);
     }
 }
--- app/src/main/java/com/example/android/vidmeclient/model/remote/IFeaturedDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/IFeaturedDataSource.java
@@ -1,8 +1,15 @@
 package com.example.android.vidmeclient.model.remote;
 
+import com.example.android.vidmeclient.model.entities.FeaturedContentResponse;
+
+import rx.Observable;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface IFeaturedDataSource {
+
+    Observable<FeaturedContentResponse> getFeaturedContent(int lim, int off);
+
 }
