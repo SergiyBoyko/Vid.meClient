67a87522722271aa53d20f707f0fa5a77baff495
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 21:36:46 +0200

    Implemented data source for new content

--- app/src/main/java/com/example/android/vidmeclient/model/remote/INewDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/INewDataSource.java
@@ -1,8 +1,15 @@
 package com.example.android.vidmeclient.model.remote;
 
+import com.example.android.vidmeclient.model.entities.ContentResponse;
+
+import rx.Observable;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface INewDataSource {
+
+    Observable<ContentResponse> getNewContent(int lim, int off);
+
 }
--- app/src/main/java/com/example/android/vidmeclient/model/remote/NewDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/NewDataSource.java
@@ -1,6 +1,9 @@
 package com.example.android.vidmeclient.model.remote;
 
 import com.example.android.vidmeclient.api.VidMeApi;
+import com.example.android.vidmeclient.model.entities.ContentResponse;
+
+import rx.Observable;
 
 /**
  * Created by fbrsw on 27.11.2017.
@@ -11,5 +14,10 @@ public class NewDataSource implements INewDataSource {
 
     public NewDataSource(VidMeApi vidMeApi) {
         this.vidMeApi = vidMeApi;
+    }
+
+    @Override
+    public Observable<ContentResponse> getNewContent(int lim, int off) {
+        return vidMeApi.getNewContent(lim, off);
     }
 }
