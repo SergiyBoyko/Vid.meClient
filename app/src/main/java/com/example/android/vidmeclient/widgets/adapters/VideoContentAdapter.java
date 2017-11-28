c4cd2110b9ae1781809587a244dca14e488bf068
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 20:35:16 +0200

    Added progress bar while video image loading

--- app/src/main/java/com/example/android/vidmeclient/widgets/adapters/VideoContentAdapter.java
+++ app/src/main/java/com/example/android/vidmeclient/widgets/adapters/VideoContentAdapter.java
@@ -3,14 +3,20 @@ package com.example.android.vidmeclient.widgets.adapters;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.android.vidmeclient.R;
 import com.example.android.vidmeclient.model.entities.Video;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -28,7 +34,7 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
 
     public VideoContentAdapter(Context context, List<Video> videos) {
         this.context = context;
-        this.videos = videos;
+        this.videos = videos != null ? videos : new ArrayList<Video>();
     }
 
     @Override
@@ -44,6 +50,19 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
         Glide
                 .with(context)
                 .load(videos.get(position).getThumbnailUrl())
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(holder.videoImage);
     }
 
@@ -53,8 +72,8 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
         return videos.size();
     }
 
-    public void setVideos(List<Video> videos) {
-        this.videos = videos;
+    public void addVideos(List<Video> videos) {
+        this.videos.addAll(videos);
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -64,6 +83,8 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
         ImageView videoImage;
         @BindView(R.id.like_count)
         TextView videoLikesCount;
+        @BindView(R.id.progress)
+        ProgressBar progressBar;
 
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_video, parent, false));
