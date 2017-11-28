86141a3b085cdc43cc508c821b02e293b52472a6
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 14:40:02 +0200

    Added imlemention root fragment

--- app/src/main/java/com/example/android/vidmeclient/ui/fragments/RootFragment.java
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/RootFragment.java
@@ -1,8 +1,36 @@
 package com.example.android.vidmeclient.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.vidmeclient.R;
+
 /**
  * Created by fbrsw on 28.11.2017.
  */
 
-public class RootFragment {
+public class RootFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		/* Inflate the layout for this fragment */
+        View view = inflater.inflate(R.layout.root_fragment, container, false);
+
+        FragmentTransaction transaction = getFragmentManager()
+                .beginTransaction();
+		/*
+		 * When this container fragment is created, we fill it with our first
+		 * "real" fragment
+		 */
+        transaction.replace(R.id.root_frame, new LoginFragment());
+
+        transaction.commit();
+
+        return view;
+    }
 }
