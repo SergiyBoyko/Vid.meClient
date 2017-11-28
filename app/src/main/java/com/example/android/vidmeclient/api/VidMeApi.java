9e54c3494aab7373a49d89ae87129add4069fd67
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-28 15:14:31 +0200

    Added get method in api class

--- app/src/main/java/com/example/android/vidmeclient/api/VidMeApi.java
+++ app/src/main/java/com/example/android/vidmeclient/api/VidMeApi.java
@@ -1,8 +1,18 @@
 package com.example.android.vidmeclient.api;
 
+import com.example.android.vidmeclient.model.entities.FeaturedContentResponse;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface VidMeApi {
+
+    @GET("/videos/featured")
+    Observable<FeaturedContentResponse> getFeaturedContent(@Query("limit") int limit, @Query("offset") int offset);
+
 }
