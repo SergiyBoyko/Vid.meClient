8ac3125c28575625f4e3091ffa46e95c50da1af2
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 00:10:53 +0200

    Created base fragment

---
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/BaseFragment.java
@@ -0,0 +1,35 @@
+package com.example.android.vidmeclient.ui.fragments;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.support.v4.app.Fragment;
+
+import com.example.android.vidmeclient.utils.NetworkConnectionReceiver;
+
+/**
+ * Created by fbrsw on 29.11.2017.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    private NetworkConnectionReceiver receiver = new NetworkConnectionReceiver() {
+        @Override
+        public void action() {
+            networkConnected();
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        getActivity().registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(receiver);
+    }
+
+    public abstract void networkConnected();
+}
