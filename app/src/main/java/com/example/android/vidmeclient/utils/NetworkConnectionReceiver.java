84442e719052336cf6c92a9848676c45e9db29d0
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 00:07:32 +0200

    Added network broadcast receiver

---
+++ app/src/main/java/com/example/android/vidmeclient/utils/NetworkConnectionReceiver.java
@@ -0,0 +1,46 @@
+package com.example.android.vidmeclient.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+/**
+ * Created by fbrsw on 28.11.2017.
+ */
+
+public abstract class NetworkConnectionReceiver extends BroadcastReceiver {
+    private boolean connected = false;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (!isInitialStickyBroadcast()) {
+            final ConnectivityManager connMgr = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            final android.net.NetworkInfo wifi = connMgr
+                    .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+            final android.net.NetworkInfo mobile = connMgr
+                    .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if (wifi.isConnected() || mobile.isConnected()) {
+                connected = true;
+                Toast.makeText(context, "connected", Toast.LENGTH_LONG).show();
+                action();
+            } else {
+                connected = false;
+                Toast.makeText(context, "disconnected", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public abstract void action();
+
+}
