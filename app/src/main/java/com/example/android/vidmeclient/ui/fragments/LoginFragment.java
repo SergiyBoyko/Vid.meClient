de8d0103b8f0a90f71f06145605486508d528ffb
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:34:43 +0200

    Implemented login opporunity in LoginFrame

--- app/src/main/java/com/example/android/vidmeclient/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/LoginFragment.java
@@ -1,7 +1,9 @@
 package com.example.android.vidmeclient.ui.fragments;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -15,10 +17,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.vidmeclient.AppVidMe;
 import com.example.android.vidmeclient.Constants;
 import com.example.android.vidmeclient.R;
+import com.example.android.vidmeclient.di.component.AppComponent;
+import com.example.android.vidmeclient.di.component.DaggerPresentersComponent;
+import com.example.android.vidmeclient.di.module.PresentersModule;
+import com.example.android.vidmeclient.model.entities.AuthResponse;
+import com.example.android.vidmeclient.presenters.LogInPresenter;
 import com.example.android.vidmeclient.ui.activities.LaunchActivity;
 import com.example.android.vidmeclient.views.LogInView;
+import com.google.gson.Gson;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,7 +39,6 @@ import butterknife.ButterKnife;
  */
 
 public class LoginFragment extends Fragment implements LogInView {
-    private static final int REQUEST_SIGNUP = 0;
 
     @BindView(R.id.input_username)
     EditText usernameText;
@@ -39,6 +49,8 @@ public class LoginFragment extends Fragment implements LogInView {
     @BindView(R.id.link_signup)
     TextView signUpLink;
 
+    @Inject
+    LogInPresenter presenter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -50,6 +62,12 @@ public class LoginFragment extends Fragment implements LogInView {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         ButterKnife.bind(this, view);
+
+        DaggerPresentersComponent.builder()
+                .appComponent(getAppComponent())
+                .presentersModule(new PresentersModule())
+                .build()
+                .inject(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
 
@@ -63,22 +81,26 @@ public class LoginFragment extends Fragment implements LogInView {
 
             @Override
             public void onClick(View v) {
-                showText("SignUP");
+//                showText("SignUP");
 //                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
 //                startActivityForResult(intent, REQUEST_SIGNUP);
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.VID_ME_LINK));
                 startActivity(browserIntent);
 
-//                FragmentTransaction trans = getFragmentManager()
-//                        .beginTransaction();
-//                trans.replace(R.id.root_frame, new FeedFragment());
-//                trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-//                trans.addToBackStack(null);
-//                trans.commit();
+
             }
         });
 
+        presenter.setView(this);
         return view;
+    }
+
+    public AppComponent getAppComponent() {
+        return getApp().appComponent();
+    }
+
+    private AppVidMe getApp() {
+        return (AppVidMe) getActivity().getApplication();
     }
 
     private void login() {
@@ -100,17 +122,42 @@ public class LoginFragment extends Fragment implements LogInView {
 
         showText("login with " + username + " " + password);
 
-        progressDialog.hide();
+        presenter.login(username, password);
 
-        onLoginSuccess();
+        progressDialog.hide();
+        loginButton.setEnabled(true);
         // TODO: Implement authentication logic here.
 
     }
 
-    public void onLoginSuccess() {
+    @Override
+    public void onLoginSuccess(AuthResponse authResponse) {
         loginButton.setEnabled(true);
-//        showText("try step 1");\
-//        finish();
+        if (authResponse.getStatus()) {
+            showText(authResponse.getUser().getUsername() + " success");
+            saveUser(authResponse);
+            finish();
+        } else showText(authResponse.getError());
+
+    }
+
+    private void saveUser(AuthResponse authResponse) {
+        getApp().setUserProfile(authResponse);
+        Gson gson = new Gson();
+        String json = gson.toJson(authResponse);
+        SharedPreferences sharedPreferences =
+                getActivity().getPreferences(Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("user", json).apply();
+    }
+
+    private void finish() {
+        ((LaunchActivity) getActivity()).showLogOutMenuItem(true);
+
+        FragmentTransaction trans = getFragmentManager().beginTransaction();
+        trans.replace(R.id.root_frame, new FeedFragment());
+        trans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        trans.addToBackStack(null);
+        trans.commit();
     }
 
     public void onLoginFailed() {
@@ -130,12 +177,18 @@ public class LoginFragment extends Fragment implements LogInView {
         } else if (Character.isDigit(username.charAt(0))) {
             usernameText.setError("username cannot begin with digit");
             valid = false;
+        } else if (username.contains(" ")) {
+            usernameText.setError("username cannot contain space");
+            valid = false;
         } else {
             usernameText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4) {
             passwordText.setError("minimum 4 symbols");
+            valid = false;
+        } else if (password.contains(" ")) {
+            usernameText.setError("password cannot contain space");
             valid = false;
         } else {
             passwordText.setError(null);
@@ -147,4 +200,5 @@ public class LoginFragment extends Fragment implements LogInView {
     private void showText(String text) {
         Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
     }
+
 }
