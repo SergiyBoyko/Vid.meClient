2089ac495ec3aca8fa15d90d81bc647f62d1d48a
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:25:29 +0200

    Added login method in LogInDataSource

--- app/src/main/java/com/example/android/vidmeclient/model/remote/ILogInDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/ILogInDataSource.java
@@ -1,8 +1,16 @@
 package com.example.android.vidmeclient.model.remote;
 
+import com.example.android.vidmeclient.model.entities.AuthResponse;
+import com.example.android.vidmeclient.model.entities.ContentResponse;
+
+import rx.Observable;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface ILogInDataSource {
+
+    Observable<AuthResponse> login(String username, String password);
+
 }
--- app/src/main/java/com/example/android/vidmeclient/model/remote/LogInDataSource.java
+++ app/src/main/java/com/example/android/vidmeclient/model/remote/LogInDataSource.java
@@ -1,6 +1,9 @@
 package com.example.android.vidmeclient.model.remote;
 
 import com.example.android.vidmeclient.api.VidMeApi;
+import com.example.android.vidmeclient.model.entities.AuthResponse;
+
+import rx.Observable;
 
 /**
  * Created by fbrsw on 27.11.2017.
@@ -11,5 +14,10 @@ public class LogInDataSource implements ILogInDataSource {
 
     public LogInDataSource(VidMeApi vidMeApi) {
         this.vidMeApi = vidMeApi;
+    }
+
+    @Override
+    public Observable<AuthResponse> login(String username, String password) {
+        return vidMeApi.login(username, password);
     }
 }
