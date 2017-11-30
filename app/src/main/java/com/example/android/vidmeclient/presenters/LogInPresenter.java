43531ba8790f82ac991ea5cdced3e9b03344bfa0
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:26:53 +0200

    Added login method in LoginPresenter

--- app/src/main/java/com/example/android/vidmeclient/presenters/LogInPresenter.java
+++ app/src/main/java/com/example/android/vidmeclient/presenters/LogInPresenter.java
@@ -1,16 +1,31 @@
 package com.example.android.vidmeclient.presenters;
 
 import com.example.android.vidmeclient.model.remote.ILogInDataSource;
+import com.example.android.vidmeclient.utils.rx.RxErrorAction;
+import com.example.android.vidmeclient.utils.rx.RxRetryWithDelay;
+import com.example.android.vidmeclient.views.LogInView;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
-public class LogInPresenter {
+public class LogInPresenter extends BasePresenter<LogInView> {
 
     private final ILogInDataSource logInDataSource;
 
     public LogInPresenter(ILogInDataSource logInDataSource) {
         this.logInDataSource = logInDataSource;
+    }
+
+    public void login(String username, String password) {
+        subscribe(logInDataSource.login(username, password)
+                .retryWhen(new RxRetryWithDelay())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getView()::onLoginSuccess, new RxErrorAction(getView().getContext()))
+        );
     }
 }
