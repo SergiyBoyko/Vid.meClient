9c4f78d78bc560451bcc18e9a924fb5fa4305396
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:21:26 +0200

    Added method in login view

--- app/src/main/java/com/example/android/vidmeclient/views/LogInView.java
+++ app/src/main/java/com/example/android/vidmeclient/views/LogInView.java
@@ -1,8 +1,12 @@
 package com.example.android.vidmeclient.views;
 
+import com.example.android.vidmeclient.model.entities.AuthResponse;
+
 /**
  * Created by fbrsw on 27.11.2017.
  */
 
 public interface LogInView extends BaseView {
+
+    void onLoginSuccess(AuthResponse authResponse);
 }
