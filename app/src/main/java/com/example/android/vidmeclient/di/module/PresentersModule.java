8dfe0e1814579e3b411181026447292034957303
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:31:16 +0200

    Added method to provide Presenter Modules

--- app/src/main/java/com/example/android/vidmeclient/di/module/PresentersModule.java
+++ app/src/main/java/com/example/android/vidmeclient/di/module/PresentersModule.java
@@ -3,8 +3,12 @@ package com.example.android.vidmeclient.di.module;
 import com.example.android.vidmeclient.di.scope.Scope;
 import com.example.android.vidmeclient.di.scope.Scopes;
 import com.example.android.vidmeclient.model.remote.IFeaturedDataSource;
+import com.example.android.vidmeclient.model.remote.IFeedDataSource;
+import com.example.android.vidmeclient.model.remote.ILogInDataSource;
 import com.example.android.vidmeclient.model.remote.INewDataSource;
 import com.example.android.vidmeclient.presenters.FeaturedDataPresenter;
+import com.example.android.vidmeclient.presenters.FeedDataPresenter;
+import com.example.android.vidmeclient.presenters.LogInPresenter;
 import com.example.android.vidmeclient.presenters.NewDataPresenter;
 
 import dagger.Module;
@@ -19,14 +23,25 @@ public class PresentersModule {
 
     @Provides
     @Scope(Scopes.VIEW)
-    public FeaturedDataPresenter provideFeaturedPresenter(IFeaturedDataSource featuredDataSource) {
+    public FeaturedDataPresenter provideFeaturedDataPresenter(IFeaturedDataSource featuredDataSource) {
         return new FeaturedDataPresenter(featuredDataSource);
     }
 
     @Provides
     @Scope(Scopes.VIEW)
-    public NewDataPresenter provideNewPresenter(INewDataSource newDataSource) {
+    public NewDataPresenter provideNewDataPresenter(INewDataSource newDataSource) {
         return new NewDataPresenter(newDataSource);
     }
 
+    @Provides
+    @Scope(Scopes.VIEW)
+    public FeedDataPresenter provideFeedDataPresenter(IFeedDataSource feedDataSource) {
+        return new FeedDataPresenter(feedDataSource);
+    }
+
+    @Provides
+    @Scope(Scopes.VIEW)
+    public LogInPresenter provideLogInPresenter(ILogInDataSource logInDataSource) {
+        return new LogInPresenter(logInDataSource);
+    }
 }
