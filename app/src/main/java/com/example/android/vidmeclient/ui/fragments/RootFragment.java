cd091c6b56bb1ddc9170388d2cdabed183e87603
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:23:49 +0200

    Refactored rootFragment

--- app/src/main/java/com/example/android/vidmeclient/ui/fragments/RootFragment.java
+++ app/src/main/java/com/example/android/vidmeclient/ui/fragments/RootFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.vidmeclient.AppVidMe;
 import com.example.android.vidmeclient.R;
 
 /**
@@ -18,19 +19,23 @@ public class RootFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		/* Inflate the layout for this fragment */
+        /* Inflate the layout for this fragment */
         View view = inflater.inflate(R.layout.root_fragment, container, false);
+        notifyUpdates();
+        return view;
+    }
 
+    public void notifyUpdates() {
         FragmentTransaction transaction = getFragmentManager()
                 .beginTransaction();
 		/*
 		 * When this container fragment is created, we fill it with our first
 		 * "real" fragment
 		 */
-        transaction.replace(R.id.root_frame, new LoginFragment());
+        if (((AppVidMe) getActivity().getApplication()).getUserProfile() == null)
+            transaction.replace(R.id.root_frame, new LoginFragment());
+        else transaction.replace(R.id.root_frame, new FeedFragment());
 
         transaction.commit();
-
-        return view;
     }
 }
