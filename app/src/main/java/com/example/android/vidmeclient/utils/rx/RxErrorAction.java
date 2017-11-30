660add7e4fb49eb4d7790da064c25429d044ab38
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-11-30 17:23:17 +0200

    Added checking for httpException in rxErrorAction

--- app/src/main/java/com/example/android/vidmeclient/utils/rx/RxErrorAction.java
+++ app/src/main/java/com/example/android/vidmeclient/utils/rx/RxErrorAction.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.widget.Toast;
 
 import com.example.android.vidmeclient.R;
+import com.example.android.vidmeclient.model.entities.AuthResponse;
+import com.google.gson.Gson;
 
+import retrofit2.adapter.rxjava.HttpException;
 import rx.functions.Action1;
 
 /**
@@ -24,6 +27,23 @@ public class RxErrorAction implements Action1<Throwable> {
         System.out.println("13579234567891231232412416");
         throwable.printStackTrace();
 
+        if (throwable instanceof HttpException) {
+            HttpException httpException = (HttpException) throwable;
+            try {
+                if (httpException.code() / 100 == 4) {
+                    String json = httpException.response().errorBody().string();
+                    Gson gson = new Gson();
+                    AuthResponse errorDto = gson.fromJson(json, AuthResponse.class);
+                    Toast.makeText(context, errorDto.getError(), Toast.LENGTH_LONG).show();
+                } else showRequestError();
+            } catch (Exception e) {
+                Toast.makeText(context, "BIG PROBLEMS!!", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        } else showRequestError();
+    }
+
+    private void showRequestError() {
         Toast.makeText(context, context.getString(R.string.request_execution_has_failed),
                 Toast.LENGTH_SHORT).show();
     }
