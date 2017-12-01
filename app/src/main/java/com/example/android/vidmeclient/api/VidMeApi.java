0e452fbb34e033c1af1f764592d7f87bf532efc4
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-01 19:52:35 +0200

    Formatted VidMeApi class

--- app/src/main/java/com/example/android/vidmeclient/api/VidMeApi.java
+++ app/src/main/java/com/example/android/vidmeclient/api/VidMeApi.java
@@ -5,6 +5,7 @@ import com.example.android.vidmeclient.model.entities.ContentResponse;
 
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -13,17 +14,24 @@ import rx.Observable;
  */
 
 public interface VidMeApi {
-
     @GET("/videos/featured")
-    Observable<ContentResponse> getFeaturedContent(@Query("limit") int limit, @Query("offset") int offset);
+    Observable<ContentResponse> getFeaturedContent(@Query("limit") int limit,
+                                                   @Query("offset") int offset);
 
     @GET("/videos/new")
-    Observable<ContentResponse> getNewContent(@Query("limit") int limit, @Query("offset") int offset);
+    Observable<ContentResponse> getNewContent(@Query("limit") int limit,
+                                              @Query("offset") int offset);
 
     @GET("/videos/feed")
-    Observable<ContentResponse> getFeedContent(@Query("limit") int limit, @Query("offset") int offset, @Query("token") String token);
+    Observable<ContentResponse> getFeedContent(@Query("limit") int limit,
+                                               @Query("offset") int offset,
+                                               @Query("token") String token);
 
     @POST("/auth/create")
     Observable<AuthResponse> login(@Query("username") String username, @Query("password") String password);
+
+    // https://api.vid.me/video/19130214/frepost?token=0c9bc436038c407689fa7f1058cdd5ed
+//    @POST("video/{video}/frepost")
+//    Observable<AuthResponse> like(@Path("video") String videoId, @Query("token") String token);
 
 }
