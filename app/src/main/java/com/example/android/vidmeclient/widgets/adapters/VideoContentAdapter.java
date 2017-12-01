a66047b9265ce1a981f9d9bb3c010548a6033f1d
SergiyBoyko <developer.sergiyboyko@gmail.com>
2017-12-01 17:38:56 +0200

    Added video onclick listener in adapter

--- app/src/main/java/com/example/android/vidmeclient/widgets/adapters/VideoContentAdapter.java
+++ app/src/main/java/com/example/android/vidmeclient/widgets/adapters/VideoContentAdapter.java
@@ -1,20 +1,25 @@
 package com.example.android.vidmeclient.widgets.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.example.android.vidmeclient.Constants;
 import com.example.android.vidmeclient.R;
 import com.example.android.vidmeclient.model.entities.Video;
+import com.example.android.vidmeclient.ui.activities.VideoPlayerActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,6 +69,7 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
                     }
                 })
                 .into(holder.videoImage);
+
     }
 
     @Override
@@ -85,11 +91,28 @@ public class VideoContentAdapter extends RecyclerView.Adapter<VideoContentAdapte
         TextView videoLikesCount;
         @BindView(R.id.progress)
         ProgressBar progressBar;
+        @BindView(R.id.action_button)
+        Button playButton;
 
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.item_video, parent, false));
 
             ButterKnife.bind(this, itemView);
+
+            View.OnClickListener listener = v -> {
+                Context context1 = v.getContext();
+                try {
+                    int index = getAdapterPosition();
+                    Intent intent = new Intent(context1, VideoPlayerActivity.class);
+                    intent.putExtra(Constants.VIDEO_ID, videos.get(index).getCompleteUrl());
+                    context1.startActivity(intent);
+                } catch (Exception e) {
+                    Toast.makeText(context1, "Error", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            };
+            videoImage.setOnClickListener(listener);
+            playButton.setOnClickListener(listener);
         }
     }
 }
